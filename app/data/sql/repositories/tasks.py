bb2c432c252d8d1d3bd74deb9e53c6a2c26df2b1
ugabiga <ugabiga@gmail.com>
2019-11-09 23:15:21 +0900

    Add get method to task sql repository

--- app/data/sql/repositories/tasks.py
+++ app/data/sql/repositories/tasks.py
@@ -1,9 +1,21 @@
+from typing import List
+
 from app.core.repositories.tasks import TaskRepository
 from app.data.sql.models.tasks import Task
 from app.extensions.database import sql_session
 
 
 class TaskSQLRepository(TaskRepository):
+    def get(self, previous_id: int, limit: int) -> List[Task]:
+        query = sql_session.query(Task)
+
+        if previous_id > 0:
+            query = query.filter(Task.id > previous_id)
+
+        tasks = query.order_by(Task.id).limit(limit).all()
+
+        return [task.to_entity() for task in tasks]
+
     def create_task(self, user_id: int, title: str, contents: str) -> Task:
         task = Task(user_id=user_id, title=title, contents=contents)
         sql_session.add(task)
--- tests/data/sql/repositories/test_tasks.py
+++ tests/data/sql/repositories/test_tasks.py
@@ -8,3 +8,17 @@ from app.data.sql.repositories.tasks import TaskSQLRepository
 def test_create_task(app: Flask, session: scoped_session, repo: TaskSQLRepository) -> None:
     task = repo.create_task(1, "hello", "this is contents")
     assert type(task) is Task
+
+
+def test_get_task_without_previous_id(app: Flask, session: scoped_session, repo: TaskSQLRepository) -> None:
+    # Create Sample Data
+    [repo.create_task(i, f"{i}", f"{i}") for i in range(0, 10)]
+
+    tasks = repo.get(0, 5)
+
+    # Assert limit count
+    assert len(tasks) == 5
+
+    # Assert type
+    for task in tasks:
+        assert type(task) is Task

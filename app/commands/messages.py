43cb581913700e8e2ef2439f7815b01049b611d8
ugabiga <ugabiga@gmail.com>
2021-02-14 02:46:20 +0900

    Add message consuming test command

--- app/commands/messages.py
+++ app/commands/messages.py
@@ -1,23 +1,46 @@
+from datetime import datetime
+from time import sleep
+
+import click
+import inject
 from flask import current_app
 
+from app.extensions.database import sql
+from core.repositories.tasks import TaskRepository
+from core.repositories.users import UserRepository
 
-@current_app.cli.command("run-producer")
-def run_producer():
-    from app.extensions.messages.producer import MessageProducer
+
+@current_app.cli.command("data")
+def create_data():
+    sql.drop_all()
+    sql.create_all()
+    user_repo = inject.instance(UserRepository)
+    task_repo = inject.instance(TaskRepository)
+
+    user = user_repo.create_user("sample")
+    task_repo.create_task(user_id=user.id, title="sample", contents="contents")
+
+
+@click.argument("topic")
+@current_app.cli.command("producer")
+def run_producer(topic: str):
+    from core.messages.producer import MessageProducer
 
     producer = MessageProducer()
     producer.init_app("")
-    for _ in range(100):
-        producer.send("test", value={"result": True})
-        print("sent message")
+    while True:
+        producer.send(f"test_{topic}", value={"result": True})
+        print(f"sending topic=={topic} at=={datetime.now()}")
+        sleep(0.1)
 
 
-@current_app.cli.command("run-consumer")
-def run_consumer():
-    from app.extensions.messages.consumer import MessageConsumer
+@click.argument("topic")
+@current_app.cli.command("consumer")
+def run_consumer(topic: str):
+    from core.messages.consumer import MessageConsumer
 
     consumer = MessageConsumer()
-    consumer.init_app("", "test")
+    consumer.init_app("", f"test_{topic}")
 
     while True:
         message = consumer.get_message()
@@ -26,5 +49,7 @@ def run_consumer():
             print(".")
             continue
 
-        print(message.__dict__())
+        print(f"processing topic=={topic} at=={message.timestamp}")
+        user_repo = inject.instance(UserRepository)
+        user_repo.update_user(1, f"new_{topic}")
         consumer.commit_message(message)

205188e62ff2735a06e8a764c783358983a02058
ugabiga <ugabiga@gmail.com>
2021-02-21 00:48:45 +0900

    Refactor message consumer, producer connection

--- core/extensions/messages/consumer.py
+++ core/extensions/messages/consumer.py
@@ -28,37 +28,48 @@ class MessageConsumerDto:
     def __dict__(self):
         return asdict(self)
 
+    @classmethod
+    def from_kafka_message(cls, message: ConsumerRecord):
+        return MessageConsumerDto(*message)
+
 
 class MessageConsumer:
     def __init__(self, config: dict):
-        bootstrap_servers = MessageHostResolver.make_from_dict(
+        self.__bootstrap_servers = MessageHostResolver.make_from_dict(
             config
         ).get_bootstrap_servers()
-        self.__consumer: KafkaConsumer = KafkaConsumer(
-            bootstrap_servers=bootstrap_servers,
-            auto_offset_reset="earliest",
-            enable_auto_commit=False,
-            group_id=config["MESSAGE_CONSUMER_GROUP_ID"],
-            value_deserializer=lambda x: json.loads(x.decode("utf-8")),
-            consumer_timeout_ms=1000,
-        )
+        self.__group_id = config["MESSAGE_CONSUMER_GROUP_ID"]
+        self.__consumer: Optional[KafkaConsumer] = None
 
     def set_topics(self, topics=()) -> None:
+        self.__connect()
         self.__consumer.unsubscribe()
         self.__consumer.subscribe(topics=topics)
 
-    def __convert_message_to_dict(self, message: ConsumerRecord):
-        return MessageConsumerDto(*message)
-
     def get_message(self) -> Optional[MessageConsumerDto]:
+        self.__connect()
         for message in self.__consumer:
             if isinstance(message, ConsumerRecord):
-                return self.__convert_message_to_dict(message=message)
+                return MessageConsumerDto.from_kafka_message(message)
             return message
 
         return None
 
     def commit_message(self, dto: MessageConsumerDto) -> None:
+        self.__connect()
         topic_partition = TopicPartition(dto.topic, dto.partition)
         offsets = {topic_partition: OffsetAndMetadata(dto.offset + 1, None)}
         self.__consumer.commit(offsets)
+
+    def __connect(self):
+        if self.__consumer is not None:
+            return
+
+        self.__consumer: KafkaConsumer = KafkaConsumer(
+            bootstrap_servers=self.__bootstrap_servers,
+            auto_offset_reset="earliest",
+            enable_auto_commit=False,
+            group_id=self.__group_id,
+            value_deserializer=lambda x: json.loads(x.decode("utf-8")),
+            consumer_timeout_ms=1000,
+        )
--- core/extensions/messages/producer.py
+++ core/extensions/messages/producer.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional
 
 from kafka import KafkaProducer
 
@@ -7,14 +8,21 @@ from core.extensions.messages.host_resolver import MessageHostResolver
 
 class MessageProducer:
     def __init__(self, config: dict):
-        bootstrap_servers = MessageHostResolver.make_from_dict(
+        self.__bootstrap_servers = MessageHostResolver.make_from_dict(
             config
         ).get_bootstrap_servers()
+        self.__producer: Optional[KafkaProducer] = None
+
+    def __connect(self):
+        if self.__producer is not None:
+            return
+
         self.__producer: KafkaProducer = KafkaProducer(
-            bootstrap_servers=bootstrap_servers,
+            bootstrap_servers=self.__bootstrap_servers,
             compression_type="gzip",
             value_serializer=lambda x: json.dumps(x).encode("utf-8"),
         )
 
     def send(self, topic: str, value: dict):
+        self.__connect()
         self.__producer.send(topic=topic, value=value)
